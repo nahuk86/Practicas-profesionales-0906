d3dc0a72ca6f63cd1f024eaf8513e768e348d50c
Nahuel Adrian Krauss <130124513+nahuk86@users.noreply.github.com>
2024-06-09 21:55:40 -0300

    Add files via upload

---
+++ Presentacion/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BLL;
+using DomainModel;
+
+namespace Presentacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Empleados empleados = new Empleados();
+        B_Empleados obj = new B_Empleados();
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+                        
+        }
+
+
+        void MostrarEmpleados()
+        {
+            DataTable dt = obj.MostrarEmpleados();
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MostrarEmpleados();
+        }
+    }
+}
